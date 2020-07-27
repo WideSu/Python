2311df21dcc0b29d8a2c10274d6364cdfc393044
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:17:26 +0800

    Update and rename 04. moving_average to 04. moving_average.py

--- 02. DataStructure/03. queue_stack/04. moving_average.py
+++ 02. DataStructure/03. queue_stack/04. moving_average.py
@@ -1,5 +1,7 @@
 #goal: given a stream of integers and a window size, calculated the moving average of all integers in the sliding window.
+
 #idea: add element to the queue first, and use a parameter to calculate sum of queue. then check the size and remove number out of size.
+
 #code:
 from collections import deque
 class MovingAverage(object):
@@ -13,11 +15,12 @@ class MovingAverage(object):
     if len(self.dq) > self.max_size:
       self.sum -= self.dq.popleft() #popleft will remove and return the removed value
     return self.sum / len(self.dq)
+
 #example:
 s = MovingAverage(2)
 print(s.next(1))
-  ->1.0
+  #->1.0
 print(s.next(5))
-  ->3.0
+  #->3.0
 print(s.next(6))
-  ->5.5
+  #->5.5

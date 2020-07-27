1041f0e0f510d6719351e4abdc367440d4d46efe
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:47:05 +0800

    Update and rename 02. series_dataframe to 02. series_dataframe.py

--- 03. Packages/02. Pandas/02. series_dataframe.py
+++ 03. Packages/02. Pandas/02. series_dataframe.py
@@ -5,24 +5,29 @@
 a = pd.Series([1,3,5,6,8])
 b = pd.Series(['a','b','c','d'])
 print(type(a))
-  -> class 'pandas.core.series.Series'
+  #-> class 'pandas.core.series.Series'
 print(a)
+  '''
   -> 0    1
      1    3
      2    5
      3    6
      4    8
      dtype: int64
+  '''
 print(b)
+  '''
   -> 0    a
      1    b
      2    c
      3    d
      dtype: object
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #create DataFrame:
 d = pd.DataFrame({'col1': [1,2,3,4,5,6],'col2': ['1', '2', '3', '4', '5', '6'], 'col3': ['1','2','3',4,5,6]})
 print(d)
+  '''
   ->   col1 col2 col3
    0     1    1    1
    1     2    2    2
@@ -30,10 +35,12 @@ print(d)
    3     4    4    4
    4     5    5    5
    5     6    6    6
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #find data's information:
 #basic information:
 d.info()
+  '''
   -> <class 'pandas.core.frame.DataFrame'>
      RangeIndex: 6 entries, 0 to 5
      Data columns (total 3 columns):
@@ -44,18 +51,24 @@ d.info()
       2   col3    6 non-null      object
      dtypes: int64(1), object(2)
      memory usage: 272.0+ bytes
+  '''
 #first k row:
 d.head(2) #first two rows, the first column is index
+  '''
   ->    col1 col2 col3
     0     1    1    1
     1     2    2    2
+  '''
 #last k row:
 d.tail(2) #last two rows
+  '''
   ->    col1 col2 col3
     4     5    5    5
     5     6    6    6
+  '''
 #find data's statistical information:
 d.describe()
+  '''
   ->            col1
      count  6.000000
      mean   3.500000
@@ -65,15 +78,17 @@ d.describe()
      50%    3.500000
      75%    4.750000
      max    6.000000
+  '''
 #find index information:
 d.index
-  -> RangeIndex(start=0, stop=6, step=1)
+  #-> RangeIndex(start=0, stop=6, step=1)
 #find column name and change names:
 d.columns
-  -> Index(['col1', 'col2', 'col3'], dtype='object')
+  #-> Index(['col1', 'col2', 'col3'], dtype='object')
 new_name = ['01','02','03']
 d.columns = new_name
 d.info()
+  '''
   -> <class 'pandas.core.frame.DataFrame'>
       RangeIndex: 6 entries, 0 to 5
       Data columns (total 3 columns):
@@ -82,8 +97,8 @@ d.info()
       03    6 non-null object
       dtypes: int64(1), object(2)
       memory usage: 272.0+ bytes
-**
+  '''
 new_name = ['01','02','03','04']
 d.columns = new_name
-  -> ValueError: Length mismatch: Expected axis has 3 elements, new values have 4 elements
+  #-> ValueError: Length mismatch: Expected axis has 3 elements, new values have 4 elements
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------

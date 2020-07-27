2500524186b29c98cfd2d517d25a605a40d4f761
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:52:25 +0800

    Update and rename 05. data_preprocessing_duplication to 05. data_preprocessing_duplication.py

--- 03. Packages/02. Pandas/05. data_preprocessing_duplication.py
+++ 03. Packages/02. Pandas/05. data_preprocessing_duplication.py
@@ -2,45 +2,59 @@ df = pd.DataFrame({'col1':['A','A','C','B','A','B','A','C'],'col2':[11,54,67,25,
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #check if a col has duplication:
 np.where(df['col1'].duplicated()) #this will return the index that duplicates occurs
-  -> (array([1, 4, 5, 6, 7]),)
+  #-> (array([1, 4, 5, 6, 7]),)
 print(df.iloc[np.where(df['col1'].duplicated())]) #with duplicates
+  '''
   ->  col1  col2  col3
     1    A    54     8
     4    A    29     6
     5    B    44     7
     6    A     9     9
     7    C    20     9
+  '''
 print(df.iloc[np.where(df['col2'].duplicated())]) #with no duplicates
+  '''
   ->Empty DataFrame
     Columns: [col1, col2, col3]
     Index: []
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #create duplicate row:
 df1 = df
 df1.loc[len(df1)] = df1.iloc[0]
 print(df1.iloc[[0,-1]])
+  '''
   ->  col1  col2  col3
     0    A    11     1
     8    A    11     1
+  '''
 print(df1.iloc[np.where(df1[['col1','col2','col3']].duplicated())])
+  '''
   ->  col1  col2  col3
     8    A    11     1
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #check duplicate row (exact same row):
 print(df[df.duplicated()])
+  '''
   ->Empty DataFrame
     Columns: [col1, col2, col3]
     Index: []
+  '''
 df1 = df
 df1.loc[len(df1)] = df1.iloc[0]
 print(df1[df1.duplicated()])
+  '''
   ->  col1  col2  col3
     8    A    11     1
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #remove duplicate:
 df2 = df1.drop_duplicates() #this function does not operate on the data, so that we need to point the function to a new variable
 print(df2.iloc[[0,-1]])
+  '''
   ->  col1  col2  col3
     0    A    11     1
     7    C    20     9
+  '''
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------

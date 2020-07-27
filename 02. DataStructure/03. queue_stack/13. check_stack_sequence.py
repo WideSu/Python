709702ea8e3410d335593fbfd59928114a288309
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:23:01 +0800

    Update and rename 13. check_stack_sequence to 13. check_stack_sequence.py

--- 02. DataStructure/03. queue_stack/13. check_stack_sequence.py
+++ 02. DataStructure/03. queue_stack/13. check_stack_sequence.py
@@ -1,5 +1,7 @@
 #goal:Given two sequences pushed and popped with distinct values, return true if and only if this could have been the result of a sequence of push and pop operations on an initially empty stack.
+
 #example: pushed = [1,2,3,4,5], popped =[4,5,3,2,1] -> true (push 1,2,3,4 then pop 4, then push 5, and pop all)
+
 #code:
 def stack_sequence_check(pushed,popped):
   s, next_consumed = [], 0

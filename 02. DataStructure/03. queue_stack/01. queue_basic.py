3a67ec04089f0bdf8a5877bae7de0007edd399c5
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:15:13 +0800

    Update and rename 01. queue_basic to 01. queue_basic.py

--- 02. DataStructure/03. queue_stack/01. queue_basic.py
+++ 02. DataStructure/03. queue_stack/01. queue_basic.py
@@ -1,5 +1,7 @@
 #idea: queue is a datastructure that all elements follow first-in-first-out policy.
+
 #what the code should achieve: add element to the queue, remove element in the queue, check if queue is empty, find the next removed element
+
 #code:
 class queue(object):
   def __init__(self):
@@ -20,15 +22,17 @@ class queue(object):
     if self.empty():
       return None
     return self.items[0]
+
 #time complexity: O(1)
+
 #example:
 q = queue()
 q.enqueue(5)
 q.enqueue(7)
 q.enqueue(11)
 print(q.empty())
-  ->False
+  #->False
 print(q.front())
-  ->5
+  #->5
 print(q.dequeue())
-  ->5
+  #->5

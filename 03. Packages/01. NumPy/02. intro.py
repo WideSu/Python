ce775c9cb552ff4fc5c4da7f3d959385615a4dae
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:36:13 +0800

    Update and rename 02. intro to 02. intro.py

--- 03. Packages/01. NumPy/02. intro.py
+++ 03. Packages/01. NumPy/02. intro.py
@@ -6,19 +6,19 @@
 lst = [[1, 2, 3], [4, 5, 6]] # 2x3
 a = np.array(lst) # create array from lists
 print(type(a)) 
-  -> class 'numpy.ndarray'
+  #-> class 'numpy.ndarray'
 print(a)
-  -> [[1 2 3]
+  #-> [[1 2 3]
       [4 5 6]]
 print(a[0]) # first row
-  -> [1 2 3] #type: 'numpy.ndarray'
+  #-> [1 2 3] #type: 'numpy.ndarray'
 print(a[0][1]) # first row, second column
-  -> 2 #type: 'numpy.int64'
+  #-> 2 #type: 'numpy.int64'
 print(a.shape) # 2 x 3
-  -> (2,3)
+  #-> (2,3)
 print(a.ndim) # 2 dimensions
-  -> 2 #a fast way to find number of dimension is to find how many [ the outest matrix has 
+  #-> 2 #a fast way to find number of dimension is to find how many [ the outest matrix has 
 print(a.dtype) # data type of the elements of array
-  -> int
+  #-> int
 print(a.size) # total number of elements in the matrix
-  -> 6
+  #-> 6

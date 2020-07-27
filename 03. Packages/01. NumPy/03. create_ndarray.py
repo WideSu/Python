5aa0c41aace78a8ff13360fe52fd1393fcf4e5ab
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-27 22:38:03 +0800

    Update 03. create_ndarray.py

--- 03. Packages/01. NumPy/03. create_ndarray.py
+++ 03. Packages/01. NumPy/03. create_ndarray.py
@@ -13,21 +13,29 @@ print(c.shape)
 
 d = np.zeros((2, 3)) # all zeros
 print(d)
-  #-> [[0. 0. 0.]
+  '''
+  -> [[0. 0. 0.]
       [0. 0. 0.]]
+  '''
 
 e = np.ones((2, 3)) # all ones
 print(e)
-  #-> [[1. 1. 1.]
+  '''
+  -> [[1. 1. 1.]
       [1. 1. 1.]]
+  '''
      
 f = np.full((2, 3), 9) # constant matrix, (2,3) shape with number 9
 print(f)
-  #-> [[9 9 9]
+  '''
+  -> [[9 9 9]
       [9 9 9]]
+  '''
 
 g = np.eye(3) # 3 x 3 identity matrix (1 on main diagonal and 0 on other positions)
 print(g)
-  #-> [[1. 0. 0.]
+  '''
+  -> [[1. 0. 0.]
       [0. 1. 0.]
       [0. 0. 1.]]
+  '''

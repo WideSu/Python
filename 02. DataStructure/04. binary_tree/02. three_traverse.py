39b14e18f28ce71c0e69df0cf9752b0fe457b46c
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-07-30 19:39:47 +0800

    Update 02. three_traverse.py

--- 02. DataStructure/04. binary_tree/02. three_traverse.py
+++ 02. DataStructure/04. binary_tree/02. three_traverse.py
@@ -1,1 +1,24 @@
-
+#pre order: first traverse root then go to its left, until no left node, then traverse right.
+def preorder(root):
+  res = []
+  helper(root,res)
+  return res
+def helper(root,res):
+  if not root:
+    return
+  res.append(root.val)
+  helper(root.left,res)
+  helper(root.right,res)
+  
+#in order: first traverse left node, then root itself and right at last.
+def inorder(root):
+  res = []
+  helper(root,res)
+  return res
+def helper(root,res):
+  if not root:
+    return
+  helper(root.left,res)
+  res.append(root.val)
+  helper(root.right,res)
+  

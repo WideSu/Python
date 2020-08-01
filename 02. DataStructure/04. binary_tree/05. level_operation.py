da4618cbe6a8c796ef416508e18a10af1a234902
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-01 21:19:52 +0800

    Update 05. level_operation.py

--- 02. DataStructure/04. binary_tree/05. level_operation.py
+++ 02. DataStructure/04. binary_tree/05. level_operation.py
@@ -1,1 +1,21 @@
+#Q1: print the binary tree in the level order
+def level_print(root):
+  q = [root] #represent the current level
+  next = [] #represent the next level below current
+  line = [] #content to print
+  while q:
+    head = q.pop(0)
+    if head.left:
+      next.append(head.left)
+    if head.right:
+      next.append(head.right)
+    line.append(head.val)
+    if not q:
+      print(line)
+      if next:
+        q = next
+        next = []
+        line = []
+#complexity: time:O(n), space:O(max(len(q)))
 
+#Q2: 

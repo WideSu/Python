34a0f958a425f85783edbf01ccbeaf6bf701d7c1
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-02 10:47:28 +0800

    Update 09. max_in_stack.py

--- 02. DataStructure/03. queue_stack/09. max_in_stack.py
+++ 02. DataStructure/03. queue_stack/09. max_in_stack.py
@@ -1,1 +1,19 @@
-
+class Stack:
+  def __init__(self):
+    self.stack = []
+  def is_empty(self):
+    return len(self.stack) == 0
+  def max(self):
+    if not self.is_empty()：
+      return self.stack[-1][1]
+    raise Exception('max():empty stack')
+  def push(self,x):
+    tmp = x
+    if not self.is_empty():
+      tmp = max(tmp,self.max())
+    self.stack.append((x,tmp))
+  def pop(self):
+    if self.is_empty():
+      raise Exception('pop(): empty stack')
+    elem = self.stack.pop()
+    return elem[0]

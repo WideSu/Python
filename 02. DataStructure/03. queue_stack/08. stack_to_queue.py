53269f46a0ef1265a7a3cae4f0572632a1e5df46
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-02 09:42:55 +0800

    Update 08. stack_to_queue.py

--- 02. DataStructure/03. queue_stack/08. stack_to_queue.py
+++ 02. DataStructure/03. queue_stack/08. stack_to_queue.py
@@ -1,1 +1,23 @@
-
+#use two stacks to achieve queue
+class Queue:
+  def __init__(self):
+    self.s1 = []
+    self.s2 = []
+    self.head = None
+  def enqueue(self,x):
+    if not self.s1:
+      self.head = x
+    self.s1.append(x)
+  def dequeue(self):
+    if not self.s2:
+      while self.s1:
+        self.s2.append(self.s1.pop())
+    retturn self.s2.pop()
+  def is_empty(self):
+    return not self.s1 and not self.s2
+  def peek(self):
+    if self.s2:
+      reyirm self.s2[-1]
+    return self.head
+  def size(self):
+    return len(self.s1) + len(self.s2)

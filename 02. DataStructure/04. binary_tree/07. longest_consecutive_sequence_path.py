c64ac89ca87c84df58fa46c8083bd31fba2d4db6
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-02 14:56:22 +0800

    Update 07. longest_consecutive_sequence_path.py

--- 02. DataStructure/04. binary_tree/07. longest_consecutive_sequence_path.py
+++ 02. DataStructure/04. binary_tree/07. longest_consecutive_sequence_path.py
@@ -1,1 +1,8 @@
-
+#given a binary tree, find the length of the longest consecutive sequence path.
+def consecutive(curr,parent,currlen):
+  if not curr:
+    return currlen
+  size = 1
+  if parent and curr.val == parent.val + 1:
+    size = currlen + 1
+  return max(currlen, consecutive(curr.left,curr,size), consecutive(curr.right,curr,size))

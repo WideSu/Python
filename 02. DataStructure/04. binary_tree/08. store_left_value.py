8914052400f144ae03d58b0e3168ac9ddb9d3f19
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-04 10:08:56 +0800

    Update 08. store_left_value.py

--- 02. DataStructure/04. binary_tree/08. store_left_value.py
+++ 02. DataStructure/04. binary_tree/08. store_left_value.py
@@ -1,1 +1,14 @@
-
+#goal: for each node in tree, store the number of nodes in its left child subtree.
+class TreeNode():
+  def __init__(self,x):
+    self.val = x
+    self.left = None
+    self.right = None
+    self.total_left = 0
+def total_subtree(node):
+  if node is None:
+    return 0
+  left_total = total_subtree(node.left)
+  right_total = total_subtree(node.right)
+  node.total_left = left_total
+  return 1 + left_total + right_total

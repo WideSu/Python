96fd8466dda09df6d4d176f6efe0f833c68756d0
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-07 20:15:58 +0800

    Update 10. minimum_height.py

--- 02. DataStructure/04. binary_tree/10. minimum_height.py
+++ 02. DataStructure/04. binary_tree/10. minimum_height.py
@@ -1,1 +1,14 @@
+#goal: given a tree, find the minimum height
 
+def get_min_height(root):
+  if root is None:
+    return 0
+  if root.left is None and root.right is None:
+    return 0
+  left = get_min_height(root.left) if root.left else float('inf')
+  right = get_min_height(root.right) if root.right else float('inf')
+  return min(left,right) + 1
+
+#complexity:
+#time: O(n)
+#space: O(height)

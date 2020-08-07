71a88a2dff321b9b938d91e57e3fcaaadafb7afe
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-07 21:11:15 +0800

    Update 11. check_binary_search_tree.py

--- 02. DataStructure/04. binary_tree/11. check_binary_search_tree.py
+++ 02. DataStructure/04. binary_tree/11. check_binary_search_tree.py
@@ -1,5 +1,6 @@
-#binary search tree: for every single node in the tree, the values in its left subtree are all smaller than its value, and the value in its right subtree are all larger than its value.
+/..g#1bnary search tree: for every single node in the tree, the values in its left subtree are all smaller than its value, and the value in its right subtree are all larger than its value.
 
+#solution1:
 def bst(root):
   if root is None:
     return True
@@ -13,6 +14,11 @@ def check_bst(root,min_val,max_val):
     return False
   return check_bst(root.left,min_val,root.val) and check_bst(root.right,root.val,max_val)
 
-#complexity:
-#time: O(n)
-#space: O(height)
+#complexity: time: O(n), space: O(height)
+
+#solution2:
+def check(root):
+  prev = [None]
+  res = [True]
+  inorder(root,prev,res)
+  

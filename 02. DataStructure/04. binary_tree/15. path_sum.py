b2584166a6354565952fccb2cd9de5537fdad718
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-08 11:15:23 +0800

    Update 15. path_sum.py

--- 02. DataStructure/04. binary_tree/15. path_sum.py
+++ 02. DataStructure/04. binary_tree/15. path_sum.py
@@ -1,1 +1,11 @@
+#goal: given a binary tree, determine if the tree has a root-to-leaf path such that adding up all values to equal target.
 
+def check(root,target):
+  return sum_path(root,0,target)
+def sum_path(curr,partial,target):
+  if curr is None:
+    return False
+  partial += curr.val
+  if curr.left is None and curr.right is None:
+    return partial == target
+  return sum_path(curr.left,partial,target) or sum_path(curr.right,partial,target)

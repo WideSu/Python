0702a3f08b70029a4440f5694e0c985e17793163
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-08 11:35:32 +0800

    Update 16. check_balanced_tree.py

--- 02. DataStructure/04. binary_tree/16. check_balanced_tree.py
+++ 02. DataStructure/04. binary_tree/16. check_balanced_tree.py
@@ -1,1 +1,16 @@
+#goal: check whether a tree is balanced (the depths of every node's left and right subtree deffer by at most 1).
 
+def check_bal(root):
+  if not root:
+    return True
+  left = get_height(root.left)
+  right = get_height(root.right)
+  if abs(left - right) > 1:
+    return False
+  return check_bal(root.left) and check_bal(root.right)
+def get_height(root):
+  if not root:
+    return 0
+  left = get_height(root.left)
+  right = get_height(root.right)
+  return 1 + max(left,right)

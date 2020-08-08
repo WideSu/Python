eb168075faeae0e2b64172bcac2a210e880e0f44
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-08 20:03:47 +0800

    Update 01. check_binary_search_tree.py

--- 02. DataStructure/05. binary_search_tree/01. check_binary_search_tree.py
+++ 02. DataStructure/05. binary_search_tree/01. check_binary_search_tree.py
@@ -1,1 +1,23 @@
+#solution1:
+def check_helper(root,prev):
+  if not root:
+    return True
+  if not check_helper(root.left,prev):
+    return False
+  if prev[0] >= root.val:
+    return False
+  prev[0] = root.val
+  return check_helper(root.right,prev)
+def check(root):
+  prev = [None]
+  return check_helper(root,prev)
 
+#solution2:
+def check_helper(root):
+  if not root:
+    return True, None, None
+  lr,lmin,lmax = check_helper(root.left)
+  rr,rmin,rmax = check_helper(root.right)
+  return lr and rr and (lmax is None or lmax < root.val) and (rmin is None or root.val < rmin), lmin if lmin is not None else root.val, rmax if rmax is not None else root.val
+def check(root):
+  return check_helper(root)[0]

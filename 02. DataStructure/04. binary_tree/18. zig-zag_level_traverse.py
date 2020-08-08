d8968f7cb559ef9db7f276e30c16f6f2f3c2431a
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-08 11:58:15 +0800

    Update 18. zig-zag_level_traverse.py

--- 02. DataStructure/04. binary_tree/18. zig-zag_level_traverse.py
+++ 02. DataStructure/04. binary_tree/18. zig-zag_level_traverse.py
@@ -1,1 +1,27 @@
-
+def zz_traverse(root):
+  if not root:
+    return []
+  reverse = True
+  res = []
+  q = [root]
+  next = []
+  line = []
+  while q:
+    head = q.pop()
+    if head.left:
+      next.append(head.left)
+    if head.right:
+      next.append(head.right)
+    line.append(head.val)
+    if not q:
+      if reverse:
+        res.extend(line[::-1]) #append a whole list2 at the end of list1
+      else:
+        res.extend(line)
+      if next:
+        q = next
+        reverse = not reverse
+        next = []
+        line = []
+  return res
+#complexity: time:O(n), space:O(len(q))

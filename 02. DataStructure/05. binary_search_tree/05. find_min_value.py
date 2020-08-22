02624f79983d496691db15b62a78ef7429547123
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-22 14:11:27 +0800

    Update 05. find_min_value.py

--- 02. DataStructure/05. binary_search_tree/05. find_min_value.py
+++ 02. DataStructure/05. binary_search_tree/05. find_min_value.py
@@ -1,1 +1,5 @@
-
+#idea: empty tree will return None, if it is not empty, then just find the left subtree because for bst, left is always the smallest one comparing with root and right sub.
+def find_min(root):
+  if not root:
+    return None
+  return find_min(root.left) or root

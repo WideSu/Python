c5ebbf5ae4a0799c4662af34e44374899a58f4ad
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-22 13:26:59 +0800

    Update 03. key_corresponding_value.py

--- 02. DataStructure/05. binary_search_tree/03. key_corresponding_value.py
+++ 02. DataStructure/05. binary_search_tree/03. key_corresponding_value.py
@@ -1,1 +1,39 @@
-class BSTNode()
+#goal: given a key, find its corresponding value
+
+#recursive solution:
+class Node():
+  def __init__(self,key,value):
+    self.key = key
+    self.value = value
+    self.left = self.right = None
+class BSTNode():
+  def __init__(self):
+    self.root = None
+  def _query(self,root,key):
+    if not root:
+      return None
+    if key < root.key:
+      return self.query(root.left,key)
+    if key > root.key:
+      return self.query(root.right,key)
+    else:
+      return root.value
+  def query(self,key):
+    return self._query(self.root,key)
+
+#iterative solution:
+class BST():
+  def __init__(self):
+    self.root = None
+  def query(self,key):
+    if not self.root:
+      return None
+    curr = self.root
+    while curr:
+      if key < curr.key:
+        curr = curr.left
+      elif key > curr.key:
+        curr = curr.right
+      else:
+        return curr.value
+    return None

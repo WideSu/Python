091be0b59118635bded14e2e0490e9725ed17f3f
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-22 15:28:31 +0800

    Update 07. last_node_smaller_than_target.py

--- 02. DataStructure/05. binary_search_tree/07. last_node_smaller_than_target.py
+++ 02. DataStructure/05. binary_search_tree/07. last_node_smaller_than_target.py
@@ -1,1 +1,13 @@
-
+def find_max(root):
+  if not root:
+    return None
+  return find_max(root.right) or root
+def find_max_small(root,target):
+  if not root:
+    return None
+  if root.key == target:
+    return find_max(root.left)
+  elif root.key > target:
+    return find_max_small(root.left,target)
+  else:
+    return find_max_small(root.right,target) or root

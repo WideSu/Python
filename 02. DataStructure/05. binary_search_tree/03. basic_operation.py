56ff2d3a29666177659be1a755cdefe86c470c30
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-22 18:18:06 +0800

    Update 03. basic_operation.py

--- 02. DataStructure/05. binary_search_tree/03. basic_operation.py
+++ 02. DataStructure/05. binary_search_tree/03. basic_operation.py
@@ -1,12 +1,11 @@
-#goal: given a key, find its corresponding value
-
-#search:
-#recursive solution:
 class Node():
   def __init__(self,key,value):
     self.key = key
     self.value = value
     self.left = self.right = None
+
+#search:
+#recursive solution:
 class BSTNode():
   def __init__(self):
     self.root = None
@@ -21,7 +20,6 @@ class BSTNode():
       return root.value
   def query(self,key):
     return self._query(self.root,key)
-
 #iterative solution:
 class BST():
   def __init__(self):
@@ -38,4 +36,76 @@ class BST():
       else:
         return curr.value
     return None
-1
+
+#insert value:
+#recursive:
+class BST():
+  def __init__(self):
+    self.root = None
+  def _insert(self,root,key,value):
+    if not root:
+      return Node(key,value)
+    if key < root.key:
+      root.left = self._insert(root.left,key,value)
+    elif key > root.key:
+      root.right = self._insert(root.right,key,value)
+    else:
+      root.value = value
+    return root
+  def insert(self,key,value):
+    self.root = self._insert(self.root,key,value)
+#iterative:
+class BST():
+  def __init__(self):
+    self.root = None
+  def insert(self,key,value):
+    if not self.root:
+      self.root = Node(key,value)
+      return
+    curr,prev,is_left = self.root,None,None
+    while curr:
+      prev = curr
+      if key < curr.key:
+        is_left = True
+        curr = curr.left
+      elif key > curr.key:
+        is_left = False
+        curr = curr.right
+      else:
+        curr.value = value
+        break
+    if not curr:
+      node = Node(key,value)
+      if is_left:
+        prev.left = node
+      else:
+        prev.right = node
+
+#delete value:
+class BST():
+  def __init__(self):
+    self.root = None
+  #delete min value
+  def delete_min(self,root):
+    if not root.left:
+      return root.right
+    root.left = self.delete_min(root.left)
+    return root
+  #delete
+  def delete(self,root,key):
+    if not root:
+      return None
+    if key < root.key:
+      root.left = self.delete(root.left,key)
+    elif key > root.key:
+      root.right = self.delete(root.right,key)
+    else:
+      if not root.right:
+        return root.left
+      if not root.left:
+        return root.right
+      t = root
+      root = self.min(t.right)
+      root.right = self.delete_min(t.right)
+      root.left = t.left
+    return root

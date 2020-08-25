37e6d6249cf8db973691dab1f9988be548848eb6
Shawn Yin <64532021+shawn-yin128@users.noreply.github.com>
2020-08-25 17:34:52 +0800

    Update 04. k_smallest.py

--- 02. DataStructure/06. heap/04. k_smallest.py
+++ 02. DataStructure/06. heap/04. k_smallest.py
@@ -2,8 +2,10 @@
 goal: find smallest k elements from an unsorted array of size n
 idea: first heapify the array, and then call pop() k times
 '''
+
+#min heap case:
 import heapq
-def kSmallest(array,k):
+def k_smallest(array,k):
   if nor array:
     return []
   res = []
@@ -12,3 +14,16 @@ def kSmallest(array,k):
     res.append(heapq.heappop(array))
   return res
   
+#max heap case:
+def k_smallest(array,k):
+  if not array:
+    return array
+  if k >= len(array):
+    return array
+  res = [-elem for elem in array[0:k]]
+  heapq.heapify(res)
+  for i in range(k,len(array)):
+    if array[i] < -res[0]:
+      heapq.heappop(res)
+      heapq.heappush(res,-array[i])
+  return [-elem for elem in res]
